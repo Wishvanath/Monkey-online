8bb60eb22bcc1bc552b69fa0e7ad85ecc410ac8d
Wishvanath SMG <anandwishvanath@gmail.com>
2020-01-03 15:01:00 +0530

    Updates

---
+++ precache-manifest.bcaa61a8a390e5ad6d200199abb911c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c766c361ceea20c97037279104db12f",
+    "url": "/Monkey-online/index.html"
+  },
+  {
+    "revision": "45f049def0ea349b9cf9",
+    "url": "/Monkey-online/static/css/main.aadcfbbf.chunk.css"
+  },
+  {
+    "revision": "8e1b2d0f83e875d17d3f",
+    "url": "/Monkey-online/static/js/2.3e89ccc1.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Monkey-online/static/js/2.3e89ccc1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "45f049def0ea349b9cf9",
+    "url": "/Monkey-online/static/js/main.bcacccaa.chunk.js"
+  },
+  {
+    "revision": "3b5bed3158596572b799",
+    "url": "/Monkey-online/static/js/runtime-main.459978cf.js"
+  }
+]);
